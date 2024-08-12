ffc1097cc54c9445a0c01bca18bd28971856c207
chuonghdh <hienchuong256@gmail.com>
2024-08-12 21:30:47 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+'''
+st.title('Hello, Streamlit!')
+st.write('This is a simple web app with Streamlit.')
+
+number = st.slider('Pick a number', 0, 100)
+st.write('You picked:', number)
+'''
+
+import streamlit as st
+
+def main():
+
+    pages = {
+        "Do Your Test": [
+            st.Page("Do_Test/all_tests_list.py", title="List of all tests"),
+            st.Page("Do_Test/your_last_tests.py", title="Your last taken tests"),
+        ],
+        "Manage Your Test": [
+            st.Page("Manage_Test/create_test.py", title="Create a new test"),
+            st.Page("Manage_Test/edit_test.py", title="Edit current test"),
+        ],
+    }
+
+    pg = st.navigation(pages)
+    pg.run()
+
+if __name__ == "__main__":
+    main()
