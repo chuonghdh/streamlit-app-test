b9d12af59e418842fb7b376888462d629131763c
chuonghdh <hienchuong256@gmail.com>
2024-08-31 14:50:40 +0700

    try 3 - fix bug show list

--- Manage_Test/edit_test.py
+++ Manage_Test/edit_test.py
@@ -110,7 +110,7 @@ def display_image_or_text(link, column, size=IMAGE_SIZE):
 def show_data_table():
     """Display the data table with rename, delete, and edit question options."""
     df = read_csv_file(CSV_FILE_PATH)
-    st.write("debug:" df["TestID"])
+    st.write(f"debug:{df["TestID"]}")
     if df.empty:
         st.write("No tests available.")
         return
