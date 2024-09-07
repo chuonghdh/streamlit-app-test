1065d51d874eb142d5ae3d19474a9c8267823381
chuonghdh <hienchuong256@gmail.com>
2024-09-08 01:26:51 +0700

    update passkey function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 import common as cm
+import time
 
 def set_custom_css():
     """Set custom CSS for wider table, row borders, and hover effects."""
@@ -41,6 +42,9 @@ def set_custom_css():
         unsafe_allow_html=True,
     )
 
+# Define the correct passkey
+correct_passkey = "class4vn"
+
 def main():
     prd_data_path = 'prd_Data' # prd_Data file only available in PRD evironment
     if not os.path.exists(prd_data_path):
@@ -48,6 +52,10 @@ def main():
         cm.initialize_data()
     
     set_custom_css()
+
+    # Create a state variable to keep track of whether the passkey has been validated
+    if 'passkey_validated' not in st.session_state:
+        st.session_state.passkey_validated = False
     
     # Define pages with their titles and file paths
     pages = {
@@ -67,9 +75,8 @@ def main():
         st.session_state.url = 'Do_Test/all_tests_list.py'
         st.rerun()  # Reload the page to reflect the new selection
     if st.sidebar.button('Edit current test'):
-        st.session_state.page = 'table'
-        st.session_state.url = 'Manage_Test/edit_test.py'
-        st.rerun()  # Reload the page to reflect the new selection
+        st.session_state.page  = 'input_passkey'
+        st.rerun()
     if st.sidebar.button('Backup tests data'):
         st.session_state.page = 'backup'
         st.session_state.url = 'Manage_Test/backup_tests.py'
@@ -77,11 +84,27 @@ def main():
 
     # Display the selected page
     if "page" in st.session_state:
-        st.write(f"Selected Page: {st.session_state.page}") #IMPORTANT FOR DEBUG
-        # Open the file with the correct encoding (usually UTF-8)
-        with open(st.session_state.url, 'r', encoding='utf-8') as f:
-            code = f.read()
-        exec(code, globals())  # Execute the selected page
+        if st.session_state.page  == 'input_passkey':  
+            st.write("###Warning###")
+            st.subheader("You need passkey to edit the test:")
+            passkey = st.text_input('Enter passkey:')
+            if st.button('Submit'):
+                if passkey.lower() == correct_passkey.lower():
+                    st.session_state.passkey_validated = True
+                    st.success("Passkey validated!")
+                    st.session_state.page = 'table'
+                    st.session_state.url = 'Manage_Test/edit_test.py'
+                    time.sleep(0.8)
+                    st.rerun()  # Reload the page to reflect the new selection
+                else:
+                    st.warning("Wrong passkey. Please try again.")
+        else:
+            st.write(f"Selected Page: {st.session_state.page}") #IMPORTANT FOR DEBUG
+            st.session_state.passkey_validated = False
+            # Open the file with the correct encoding (usually UTF-8)
+            with open(st.session_state.url, 'r', encoding='utf-8') as f:
+                code = f.read()
+            exec(code, globals())  # Execute the selected page
     else:
         st.session_state.page = 'test_list'
         st.session_state.url = 'Do_Test/all_tests_list.py'
